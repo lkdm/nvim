f56abd5b2468a637e4f66d5cb05efffe5900f060
Luke Martin <44519853+lkdm@users.noreply.github.com>
2024-09-24 14:55:40 +1000

    feat: use packr instead.

--- lua/lkdm/init.lua
+++ lua/lkdm/init.lua
@@ -1,2 +1,22 @@
 require("lkdm.set")
+require("lkdm.plugins")
 print("🚀 We have liftoff")
+
+
+local lspconfig = require('lspconfig')
+lspconfig.rust_analyzer.setup {
+    -- Server-specific settings. See `:help lspconfig-setup`
+    settings = {
+        ['rust-analyzer'] = {},
+    },
+}
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+    "n",
+    "<leader>a",
+    function()
+        vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+        -- or vim.lsp.buf.codeAction() if you don't want grouping.
+    end,
+    { silent = true, buffer = bufnr }
+)
