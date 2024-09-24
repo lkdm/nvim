4d63f4030d189ba2fa60673b20644b195d48a30d
Luke Martin <44519853+lkdm@users.noreply.github.com>
2024-09-24 15:50:24 +1000

    wip

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Neovim configuration
 
 - `make init`: Install dependencies for Mac OS
 - `make link`: Create symlink to repository
+
+### TODO
+
+- [ ] Use Mountaineer theme instead https://github.com/TheNiteCoder/mountaineer.vim
--- lua/lkdm/init.lua
+++ lua/lkdm/init.lua
@@ -3,3 +3,7 @@ require("lkdm.plugins")
 require("lkdm.lsp")
 require("lkdm.rust")
 print("🚀 We have liftoff")
+
+local augroup = vim.api.nvim_create_augroup
+local RustFiletypeGroup = augroup('RustFiletypeGroup', {})
+local TypescriptFiletypeGroup = augroup('TypescriptFiletypeGroup', {})
